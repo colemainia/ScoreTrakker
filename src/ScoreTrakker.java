5afaa78ec3517f65742e87f9f5ba5254fd358eae
Josiah Emmanuel Navarro <jnavarro@mines.edu>
2017-02-19 16:57:38 -0700

    Added saved java files

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 
 public class ScoreTrakker {
-
+	private ArrayList<Student> studentList;
+	
+	public void loadDataFromFile(String f) {
+		File
+	}
+	
+	public void printInOrder() {
+		
+	}
+	
+	public void processFiles() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,28 @@
 
-public class Student {
 
+public class Student implements Comparable<Student>{
+	private String name;
+	private int score;
+	
+	Student() {
+		name = "";
+		score = 0;
+	}
+	
+	Student(String s, int i) {
+		name = s;
+		score = i;
+	}
+
+	@Override
+	public int compareTo(Student s) {
+		return name.compareTo(s.name);
+	}
+
+	@Override
+	public String toString() {
+		return name + " " + score;
+	}
+	
+	
 }
