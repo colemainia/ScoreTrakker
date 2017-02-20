1c51cc7a82624f324f7236df363e4b0c9d5cea62
Coleman Hoyt <choyt@mines.edu>
2017-02-19 18:44:55 -0700

    Did part II

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -6,38 +6,54 @@ import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> studentList = new ArrayList<Student>();
-	
+	private static String[] file = {"scores.txt", "badscore.txt", "nofile.txt"};
+
 	public void loadDataFromFile(String f) throws FileNotFoundException {
+		studentList.clear();
 		FileReader file = new FileReader(f);
 		Scanner in = new Scanner(file);
-		
+
 		while(in.hasNextLine()) {
 			String first = in.next();
 			String last = in.next();
-			int score = in.nextInt();
+			String score = in.next();
+			int scoreInt = 0;
 			
-			Student nextStudent = new Student(first, last, score);
+			try {
+				scoreInt = Integer.parseInt(score);
+			} catch(NumberFormatException n) {
+				System.out.println("Error printing score of " + first + " " + last);
+			}
+
+			Student nextStudent = new Student(first, last, scoreInt);
 			studentList.add(nextStudent);
 		}
 		in.close();
-		
+
 	}
-	
+
 	public void printInOrder() {
 		Collections.sort(studentList);
-		
+
 		for (Student s : studentList) {
 			System.out.println(s.toString());
 		}
 	}
-	
-	public void processFiles(String str) throws FileNotFoundException {
-		loadDataFromFile(str);
-		printInOrder();
+
+	public void processFiles() throws FileNotFoundException {
+		for (String fn : ScoreTrakker.file) {
+			try {
+				loadDataFromFile(fn);
+				printInOrder();
+			} catch(FileNotFoundException f) {
+				System.out.println("File " + fn + " not found");
+			}
+		}
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException{
-		ScoreTrakker scoreTrakker = new ScoreTrakker();
-		scoreTrakker.processFiles("scores.txt");
+		ScoreTrakker test = new ScoreTrakker();
+		test.processFiles();
+		
 	}
 }
