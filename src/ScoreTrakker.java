71ea3bf82020e45ce79c9dd90b4a095052c1d5b9
Josiah Emmanuel Navarro <jnavarro@mines.edu>
2017-02-19 17:20:28 -0700

    Worked on loadDataFromFile

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,11 +1,25 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> studentList;
 	
-	public void loadDataFromFile(String f) {
+	public void loadDataFromFile(String f) throws FileNotFoundException {
 		FileReader file = new FileReader(f);
+		Scanner in = new Scanner(file);
+		
+		while(in.hasNextLine()) {
+			String name = in.nextLine();
+			int score = in.nextInt();
+			// Still have to add a way to split name string into first and last name
+			
+			Student nextStudent = new Student(name, score);
+			studentList.add(nextStudent);
+		}
+		
+		
 	}
 	
 	public void printInOrder() {
@@ -17,6 +31,6 @@ public class ScoreTrakker {
 	}
 	
 	public static void main(String[] args) {
-		
+
 	}
 }
