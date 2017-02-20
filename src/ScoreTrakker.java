d8f7bd4d838b2868d914b753cb5c85d77e146de6
Coleman Hoyt <choyt@mines.edu>
2017-02-19 17:04:14 -0700

    added fileReader object

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,10 +1,11 @@
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class ScoreTrakker {
 	private ArrayList<Student> studentList;
 	
 	public void loadDataFromFile(String f) {
-		File
+		FileReader file = new FileReader(f);
 	}
 	
 	public void printInOrder() {
